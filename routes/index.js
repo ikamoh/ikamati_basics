cd7f2d54763605f55872217dc5ab73fdf2964e5a
ernestikamati <ernestikamati@gmail.com>
2024-09-12 22:09:25 +0300

    Added new files

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('cmd', { title: 'Express' });
 });
 
+router.get('/git1', function(req, res, next) {
+  res.render('git-basics', { title: 'git' });
+});
+
+router.get('/git2', function(req, res, next) {
+  res.render('git-basics-2', { title: 'git 2' });
+});
+
+
 
 module.exports = router;
